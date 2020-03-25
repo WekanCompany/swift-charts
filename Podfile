243a465787b42e8dff2657c1ab5cdb164959eefa
Santhosh Kumar <santhoshk@wekancode.com>
2020-03-25 10:34:28 +0530

    initial commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Swift_Charts' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Swift_Charts
+pod 'Charts'
+
+end
---
+++ Swift_Charts/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  Swift_Charts
+//
+//  Created by Santhosh Kumar on 19/03/20.
+//  Copyright © 2020 WeKanCode. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var navController = UINavigationController()
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        let mainStoryboard = UIStoryboard.init(name: "Main", bundle: nil)
+        let viewController = (mainStoryboard.instantiateViewController(withIdentifier: "ViewController") as? ViewController)!
+        
+        navController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+
+}
+
